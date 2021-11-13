e427c2bca6c20cf0f6205d037da5e920cbc3667b
Daniele Alberti <kadmos@kadmos.com>
2021-11-13 13:01:43 +0100

    DXFREaderNET dock

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DXFReaderNET.DxfDocument dxfDocument2 = new DXFReaderNET.DxfDocument();
-            DXFReaderNET.AciColor aciColor2 = new DXFReaderNET.AciColor();
-            DXFReaderNET.Objects.RasterVariables rasterVariables2 = new DXFReaderNET.Objects.RasterVariables();
+            DXFReaderNET.DxfDocument dxfDocument1 = new DXFReaderNET.DxfDocument();
+            DXFReaderNET.AciColor aciColor1 = new DXFReaderNET.AciColor();
+            DXFReaderNET.Objects.RasterVariables rasterVariables1 = new DXFReaderNET.Objects.RasterVariables();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.dxfReaderNETControl1 = new DXFReaderNET.DXFReaderNETControl();
             this.button1 = new System.Windows.Forms.Button();
@@ -39,53 +39,55 @@
             // dxfReaderNETControl1
             // 
             this.dxfReaderNETControl1.AutoSize = true;
+            this.dxfReaderNETControl1.ContinuousHighlight = false;
             this.dxfReaderNETControl1.CustomCursor = DXFReaderNET.CustomCursorType.None;
-            dxfDocument2.ActiveLayout = "Model";
-            dxfDocument2.Comments = null;
-            aciColor2.Index = ((short)(256));
-            aciColor2.IsByBlock = false;
-            aciColor2.IsByLayer = true;
-            aciColor2.UseTrueColor = false;
-            dxfDocument2.CurrentColor = aciColor2;
-            dxfDocument2.CurrentElevation = 0D;
-            dxfDocument2.CurrentLayer = "0";
-            dxfDocument2.CurrentLineTypeName = "ByLayer";
-            dxfDocument2.CurrentLineTypeScale = 1D;
-            dxfDocument2.CurrentTextSize = 2.5D;
-            dxfDocument2.CurrentTextStyle = "STANDARD";
-            dxfDocument2.CurrentThickness = 0D;
-            dxfDocument2.DateCreated = new System.DateTime(2020, 1, 29, 9, 59, 45, 111);
-            dxfDocument2.DateLastEdited = new System.DateTime(2020, 1, 29, 9, 59, 45, 111);
-            dxfDocument2.Handle = "0";
-            dxfDocument2.IsBinary = false;
-            dxfDocument2.Modified = false;
-            dxfDocument2.Name = null;
-            dxfDocument2.Owner = null;
-            rasterVariables2.DisplayFrame = true;
-            rasterVariables2.DisplayQuality = DXFReaderNET.Objects.ImageDisplayQuality.High;
-            rasterVariables2.Handle = "2D";
-            rasterVariables2.Owner = null;
-            rasterVariables2.Units = DXFReaderNET.ImageUnits.Unitless;
-            dxfDocument2.RasterVariables = rasterVariables2;
-            dxfDocument2.ThumbnailImage = null;
-            dxfDocument2.TotalEditTime = new System.DateTime(2020, 1, 29, 0, 0, 0, 0);
-            dxfDocument2.TotalEditTimeDays = 0;
-            dxfDocument2.UserTimer = new System.DateTime(2020, 1, 29, 0, 0, 0, 0);
-            dxfDocument2.UserTimerDays = 0;
-            this.dxfReaderNETControl1.DXF = dxfDocument2;
+            this.dxfReaderNETControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            dxfDocument1.ActiveLayout = "Model";
+            dxfDocument1.Comments = null;
+            aciColor1.Index = ((short)(256));
+            aciColor1.IsByBlock = false;
+            aciColor1.IsByLayer = true;
+            aciColor1.UseTrueColor = false;
+            dxfDocument1.CurrentColor = aciColor1;
+            dxfDocument1.CurrentElevation = 0D;
+            dxfDocument1.CurrentLayer = "0";
+            dxfDocument1.CurrentLineTypeName = "ByLayer";
+            dxfDocument1.CurrentLineTypeScale = 1D;
+            dxfDocument1.CurrentTextSize = 2.5D;
+            dxfDocument1.CurrentTextStyle = "STANDARD";
+            dxfDocument1.CurrentThickness = 0D;
+            dxfDocument1.DateCreated = new System.DateTime(2020, 1, 29, 9, 59, 45, 111);
+            dxfDocument1.DateLastEdited = new System.DateTime(2020, 1, 29, 9, 59, 45, 111);
+            dxfDocument1.Handle = "0";
+            dxfDocument1.IsBinary = false;
+            dxfDocument1.Modified = false;
+            dxfDocument1.Name = null;
+            dxfDocument1.Owner = null;
+            rasterVariables1.DisplayFrame = true;
+            rasterVariables1.DisplayQuality = DXFReaderNET.Objects.ImageDisplayQuality.High;
+            rasterVariables1.Handle = "2D";
+            rasterVariables1.Owner = null;
+            rasterVariables1.Units = DXFReaderNET.ImageUnits.Unitless;
+            dxfDocument1.RasterVariables = rasterVariables1;
+            dxfDocument1.ThumbnailImage = null;
+            dxfDocument1.TotalEditTime = new System.DateTime(2020, 1, 29, 0, 0, 0, 0);
+            dxfDocument1.TotalEditTimeDays = 0;
+            dxfDocument1.UserTimer = new System.DateTime(2020, 1, 29, 0, 0, 0, 0);
+            dxfDocument1.UserTimerDays = 0;
+            this.dxfReaderNETControl1.DXF = dxfDocument1;
             this.dxfReaderNETControl1.FileName = null;
             this.dxfReaderNETControl1.HighlightMarkerColor2 = System.Drawing.Color.Magenta;
-            this.dxfReaderNETControl1.Location = new System.Drawing.Point(12, 12);
+            this.dxfReaderNETControl1.Location = new System.Drawing.Point(0, 0);
             this.dxfReaderNETControl1.Name = "dxfReaderNETControl1";
             this.dxfReaderNETControl1.ShowGridRuler = false;
-            this.dxfReaderNETControl1.Size = new System.Drawing.Size(814, 619);
+            this.dxfReaderNETControl1.Size = new System.Drawing.Size(1057, 643);
             this.dxfReaderNETControl1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(841, 12);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(194, 43);
+            this.button1.Size = new System.Drawing.Size(146, 31);
             this.button1.TabIndex = 1;
             this.button1.Text = "Add gradient hatches";
             this.button1.UseVisualStyleBackColor = true;
